e14306ee1c0ae7905022c0fddc7bca4241ffd716
Jordan Hart <p22jhart@fairfieldprep.org>
2022-01-27 07:18:11 -0800

    practice quiz

---
+++ q2.py
@@ -0,0 +1,10 @@
+from math import factorial
+
+
+num = int(input("enter a number: "))
+
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial = factorial * i
+    print("factorial of ", num , "is", factorial)
